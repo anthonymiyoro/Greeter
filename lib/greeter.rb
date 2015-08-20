72af88b3558e94486b9b299657900d3f94aac599
anthonymiyoro <amiyoro@gmail.com>
2015-08-20 23:11:14 +0300

    Added code that imports html and made new html file

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -1,5 +1,13 @@
+require "erb"
+
 class Greeter
 	def call(env)
-	Rack::Response.new("Hello there!")
+	Rack::Response.new(render("index.html.erb"))
+	end
+
+#  The line below takes the HTML from the template folder
+	def render(template)
+		path = File.expand_path("../views/#{template}", __FILE__)
+		ERB.new(File.read(path)).result(binding)
 	end
 end
