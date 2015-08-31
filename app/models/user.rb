15615d1151bb7dbc4966c39224aafad5c0dff72d
anthonymiyoro <amiyoro@gmail.com>
2015-08-31 22:33:41 +0300

    Made sure that a name is present

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
     attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
     has_secure_password
+    validates :name, presence: true
 end
