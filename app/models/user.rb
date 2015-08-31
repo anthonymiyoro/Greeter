4f4ac7282ab8a03f843b6c04ef67299ada5794de
anthonymiyoro <amiyoro@gmail.com>
2015-08-31 22:34:57 +0300

    Made sure that a username is present and unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
     attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
     has_secure_password
     validates :name, presence: true
+    validates :username, uniqueness: true,
 end
