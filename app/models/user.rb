2b776caabc37643234b7f77f0c0524b668029720
anthonymiyoro <amiyoro@gmail.com>
2015-09-01 10:34:49 +0300

    Added method that generates a gravatar url and called it before the user is saved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
     has_secure_password
+    before_save :create_avatar_url
     before_validation :prep_email
     validates :name, presence: true
     validates :username, uniqueness: true,
@@ -12,4 +13,7 @@ class User < ActiveRecord::Base
     def prep_email
         self.email = self.email.strip.downcase if self.email
     end
+
+    def create_avatar_url
+        self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50}"
 end
