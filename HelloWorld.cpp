afcb1d18c858172871928be75688901f16d32a81
Anthony Miyoro <amiyoro@gmail.com>
2016-01-21 10:08:53 +0300

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -8,3 +8,18 @@ int main(){
     cout << "Hello world!" << endl;
     return 0;
 }
+
+
+// Hello World program in C++
+
+# include <iostream>
+
+using namespace std;
+
+void main(){
+	cout << "Hello World!";
+
+	system("pause");
+}
+
+
