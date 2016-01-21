84b9fe1fee7fa17350213bcb37003d3ce3aa6ef2
anthonymiyoro <amiyoro@gmail.com>
2016-01-21 07:49:45 +0300

    Added first example program

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+// Below is a preprocessor directive 
+# include <iostream>
+
+// calls the standard namespace library
+using namespace std;
+
+int main(){
+    cout << "Hello world!" << endl;
+    return 0;
+}
