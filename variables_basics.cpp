b151959b2664a5f43ab083644eaab76cdeafc079
anthonymiyoro <amiyoro@gmail.com>
2016-01-27 07:41:23 +0300

    Added notes for basic variables

---
+++ variables_basics.cpp
@@ -0,0 +1,17 @@
+ # include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// int is used to show that the variable stores an integer
+	int a = 10;
+	int b = 10;
+	
+	// all variables must have their data type listed
+	int sum = a + b;
+
+	cout << sum;
+
+	return 0;
+}
