780cf75021c37f8fec883d7e2ed9f61efecc4829
anthonymiyoro <amiyoro@gmail.com>
2016-01-27 07:22:59 +0300

    Completed Hello World program

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,26 +1,23 @@
+// BASIC LINE PRINTING PROGRAM
 // Below is a preprocessor directive 
 # include <iostream>
 
 // calls the standard namespace library
 using namespace std;
 
-int main(){
-	
-    cout << "Hello world!";
+int main()
+{
 
+// cout << is used to print out text in quotation marks
+    cout << "Hello world!" << endl;
+
+// \n can be used to skip lines	
+    cout << "Skipped a new line \n\n";
+    cout << "And me";
+
+// return 0 is then used to end a program
+    return 0;
 }
 
-// School version of Hello World
-// Hello World program in C++
-
-// # include <iostream>
-
-// using namespace std;
-
-// void main(){
-// 	cout << "Hello World!";
-
-// 	system("pause");
-// }
 
 
