f689be67408f3aa27faa24358f65ed88cb468b6b
anthonymiyoro <amiyoro@gmail.com>
2016-01-28 07:50:09 +0300

    Added variable declaration notes

---
+++ variable_declaration.cpp
@@ -0,0 +1,17 @@
+ #include <iostream>
+
+using namespace std;
+
+ int main()
+{
+	// declared 3 integer variables below
+	int a;
+	int b;
+	int sum;
+
+	// collect input from the user and stored it in a
+	cout << "Enter a number below!\n";
+	cin >> a;
+
+	return 0;
+}
