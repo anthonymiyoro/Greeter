5391680bfd1f466cdf27b1658aec9d2575b1a7d8
Anthony Miyoro <amiyoro@gmail.com>
2016-01-28 10:08:33 +0300

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -19,5 +19,64 @@ int main()
     return 0;
 }
 
+// question 1
+# include <iostream>
+using namespace std;
+
+int main()
+{
+// Question 1
+    cout << "Full Names: John Peter \n";	
+    cout << "High School Attended: Njiris High School \n";
+    cout << "Hobbies: programming, football, dancing \n\n\n";
+return 0;
+}
+
+
+// Question 2
+# include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Strathmore University, a leading University in the region, whose mission is to provide\n";
+cout << "all-round quality education in an atmosphere of freedom and responsibility; excellence in \n";
+cout << "teaching, research and scholarship; ethical and social development; and service to\n";
+cout << "society.\n\n";
+return 0;
+}
+
+
+//Question 3
+# include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "Fulltime Programme Entry Requirements are\n";
+cout <<"\t\t KCSE mean grade C+ with C+ in Mathematics and English,\n";		
+cout <<"\t\t\tKACE with 2 principal passes and 1 subsidiary pass, and a credit\n";	
+cout <<" pass in Mathematics at KCE.\n";	
+cout <<"\n";	
+cout <<"\t\t\tDiploma from recognized institutions \n\n\n";	
+return 0;
+}
+
+
+// Question 4
+# include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "Hello          in my university\n";
+cout <<"\n";	
+cout << "I am a student in FIT\n";
+cout <<"\n";
+cout <<"\t\t\t Welcome \n";	
+return 0;
+}
+
+
 
 
