1c461f68f95a05eee1326f77283ca2e9b341b49a
Anthony Miyoro <amiyoro@gmail.com>
2016-02-25 09:13:58 +0300

    Added 3rd Question

--- pointers.cpp
+++ pointers.cpp
@@ -31,3 +31,32 @@ int main(){
         cout << "z is "<< z << endl;
     return 0;
 }
+
+
+# include <string>
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+ // the pointers and variables are declared below
+int i, j;
+int *p, *q;
+// the variable i is assigned to 100
+i = 100;
+// the address of i is assigned to p
+p = &i;
+// the address of j is assigned to q
+q = &j;
+// pointer variable q is assigned to the number stored in i
+*q = i;
+// the value pointed by p is added to the value pointed by q
+*q = *p + *q;
+// returns the value pointed by q
+cout<<*q;
+return 0;
+}
+
+
+// The output is then 200
