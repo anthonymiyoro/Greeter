ebc452c00e66db3b8d815c5d157773fd6029f918
anthonymiyoro <amiyoro@gmail.com>
2016-03-30 11:00:45 +0300

    Added basic functions program

---
+++ basic_functions.cpp
@@ -0,0 +1,29 @@
+// do-while loop
+# include <iostream>
+
+
+using namespace std;
+int sum = 0;
+;
+
+//function declaration
+int calc(int length, int width, int area);
+
+
+//function that computes area
+int calc(int length, int width){
+    int area = (length * width);
+
+    cout << "the area is " << area << endl;
+    cout << "the width is " << width << endl;
+    cout << "the length is " << length << endl;
+}
+
+//main function
+int main() {
+    int rec1 = calc(5,6);
+    int rec2 = calc(10,12);
+}
+
+
+
